b8781b274a34296dedfec275f0db83dfaf3005fb
schernaya <chernaya.sofiia@gmail.com>
2021-07-18 12:38:59 +0300

    Change room price to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_173631) do
+ActiveRecord::Schema.define(version: 2021_07_18_092521) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_07_12_173631) do
 
   create_table "rooms", force: :cascade do |t|
     t.integer "capacity"
-    t.integer "price"
+    t.float "price"
     t.string "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
