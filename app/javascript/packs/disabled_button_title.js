fef37b5ee0dbb3c09ef0e6cd4433d19091e2e43a
Olha-30 <vasianovych.olha@chnu.edu.ua>
2021-07-26 19:28:26 +0300

    replace navbar in header, language link active

--- app/javascript/packs/disabled_button_title.js
+++
@@ -1,5 +0,0 @@
-var disabled_buttons = document.getElementsByClassName('disabled_button_title');
-function add_title(button){
-    button.title = 'You cannot delete service with working orders';
-};
-for(var i in disabled_buttons) add_title(disabled_buttons[i]);
