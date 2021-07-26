b13c8305da0b113bd36c772e5d425eb7fcfc2e36
Olha-30 <vasianovych.olha@chnu.edu.ua>
2021-07-26 17:12:37 +0000

    Delete disabled_button_title.js

--- app/javascript/packs/disabled_button_title.js
+++
@@ -1,4 +0,0 @@
-var disabled_button = document.getElementById('dis_but');
-disabled_button.onmouseover = function() {
-    disabled_button.title = 'You cannot delete service with working orders';
-}
