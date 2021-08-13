752ee6ca5faa7494ed78ace74c25339aae2d7220
anastass413 <anastass413@gmail.com>
2021-08-13 14:53:53 +0300

    Conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,122 +10,131 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_045617) do
+ActiveRecord::Schema.define(version: 2021_07_27_074243) do
 
-  create_table 'categories', force: :cascade do |t|
-    t.integer 'name'
-    t.string 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "categories", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'feedbacks', force: :cascade do |t|
-    t.string 'text'
-    t.integer 'user_id'
-    t.decimal 'stars'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "feedbacks", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.decimal "stars", precision: 10
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "fk_rails_c57bb6cf28"
   end
 
-  create_table 'friendly_id_slugs', force: :cascade do |t|
-    t.string 'slug', null: false
-    t.integer 'sluggable_id', null: false
-    t.string 'sluggable_type', limit: 50
-    t.string 'scope'
-    t.datetime 'created_at'
-    t.index ['slug', 'sluggable_type', 'scope'], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope', unique: true
-    t.index ['slug', 'sluggable_type'], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type'
-    t.index ['sluggable_type', 'sluggable_id'], name: 'index_friendly_id_slugs_on_sluggable_type_and_sluggable_id'
+  create_table "friendly_id_slugs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
-  create_table 'images', force: :cascade do |t|
-    t.string 'name'
-    t.string 'path'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "images", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'issues', force: :cascade do |t|
-    t.integer 'category_id'
-    t.integer 'user_id'
-    t.string 'text'
-    t.integer 'status', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "issues", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "user_id"
+    t.string "text"
+    t.integer "status", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "fk_rails_847dcee27e"
+    t.index ["user_id"], name: "fk_rails_f8f1052133"
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.date 'check_in'
-    t.date 'check_out'
-    t.float 'total_price'
-    t.integer 'room_id'
-    t.integer 'user_id'
-    t.integer 'status'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "orders", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.date "check_in"
+    t.date "check_out"
+    t.float "total_price"
+    t.integer "room_id"
+    t.integer "user_id"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "fk_rails_8d6e2128b7"
+    t.index ["user_id"], name: "fk_rails_f868b47f6a"
   end
 
-  create_table 'room_images', force: :cascade do |t|
-    t.decimal 'room_id'
-    t.decimal 'image_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'purpose'
+  create_table "room_images", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "purpose"
+    t.index ["image_id"], name: "fk_rails_e91d39bb78"
+    t.index ["room_id"], name: "fk_rails_823e921e11"
   end
 
-  create_table 'room_services', force: :cascade do |t|
-    t.integer 'room_id'
-    t.integer 'service_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "room_services", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "service_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "fk_rails_2a21909fed"
+    t.index ["service_id"], name: "fk_rails_524d1ad693"
   end
 
-  create_table 'rooms', force: :cascade do |t|
-    t.integer 'capacity'
-    t.float 'price'
-    t.string 'number'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'title'
-    t.string 'slug'
-    t.index ['slug'], name: 'index_rooms_on_slug', unique: true
+  create_table "rooms", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "capacity"
+    t.float "price"
+    t.string "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "slug"
+    t.index ["slug"], name: "index_rooms_on_slug", unique: true
   end
 
-  create_table 'services', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'icon_url'
+  create_table "services", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "icon_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'full_name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'role'
-    t.integer 'status'
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
+    t.integer "status"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'feedbacks', 'users'
-  add_foreign_key 'issues', 'categories'
-  add_foreign_key 'issues', 'users'
-  add_foreign_key 'orders', 'rooms'
-  add_foreign_key 'orders', 'users'
-  add_foreign_key 'room_images', 'images'
-  add_foreign_key 'room_images', 'rooms'
-  add_foreign_key 'room_services', 'rooms'
-  add_foreign_key 'room_services', 'services'
+  add_foreign_key "feedbacks", "users"
+  add_foreign_key "issues", "categories"
+  add_foreign_key "issues", "users"
+  add_foreign_key "orders", "rooms"
+  add_foreign_key "orders", "users"
+  add_foreign_key "room_images", "images"
+  add_foreign_key "room_images", "rooms"
+  add_foreign_key "room_services", "rooms"
+  add_foreign_key "room_services", "services"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 User.create!([
-  {full_name: "Vasyanovych Olya", email: "vasyanovych30@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$GoUKh6Z3x0Z4Bz8cNNaAIuipshfvjKvj1oASaFWASw9pGUIUiBNii", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
-  {full_name: "Chernaya Sophiia", email: "chernaya.sofiia@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$wjf3g8TQoVW1cgO6ohjIBeglfFhct5K9dX9bUz2Ibhyv8eb9oARge", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
-  {full_name: "Masiian Anastasiia", email: "anastass413@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$RKwfzAmUStzbpfewNf1KAushIv4kn6Pi/KAf1esZUfLxB3Okj7xDy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
-  {full_name: "Test User", email: "test_user@gmail.com", role: "user", status: nil, encrypted_password: "$2a$12$RuRmLgY2/lmtXwCQee4LyOcyOqeIrqf.ildW8kLM7OXdUPC8Ms9D6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil}
-])
-Feedback.create!([
-  {text: "example", user_id: 1, stars: "4.0"}
+{full_name: "Vasyanovych Olya", email: "vasyanovych30@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$GoUKh6Z3x0Z4Bz8cNNaAIuipshfvjKvj1oASaFWASw9pGUIUiBNii", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
+{full_name: "Chernaya Sophiia", email: "chernaya.sofiia@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$wjf3g8TQoVW1cgO6ohjIBeglfFhct5K9dX9bUz2Ibhyv8eb9oARge", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
+{full_name: "Masiian Anastasiia", email: "anastass413@gmail.com", role: "manager", status: nil, encrypted_password: "$2a$12$RKwfzAmUStzbpfewNf1KAushIv4kn6Pi/KAf1esZUfLxB3Okj7xDy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil},
+{full_name: "Test User", email: "test_user@gmail.com", role: "user", status: nil, encrypted_password: "$2a$12$RuRmLgY2/lmtXwCQee4LyOcyOqeIrqf.ildW8kLM7OXdUPC8Ms9D6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, confirmation_token: nil, confirmed_at: nil, confirmation_sent_at: nil, unconfirmed_email: nil}
 ])
 Image.create!([
   {name: "1", path: "1.jpg"},
@@ -20,27 +17,46 @@ Image.create!([
   {name: "10", path: "10.jpg"},
   {name: "main-page-image", path: "main-page-image.jpg"}
 ])
+RoomImage.create!([
+  {room_id: 1, image_id: 1, purpose: "cover"},
+  {room_id: 2, image_id: 2, purpose: "cover"},
+  {room_id: 3, image_id: 3, purpose: "cover"},
+  {room_id: 1, image_id: 3, purpose: nil},
+  {room_id: 1, image_id: 4, purpose: nil},
+  {room_id: 1, image_id: 5, purpose: nil},
+  {room_id: 2, image_id: 6, purpose: nil},
+  {room_id: 2, image_id: 7, purpose: nil},
+  {room_id: 3, image_id: 8, purpose: nil},
+  {room_id: 3, image_id: 9, purpose: nil},
+  {room_id: 2, image_id: 4, purpose: nil},
+  {room_id: 4, image_id: 8, purpose: "cover"},
+  {room_id: 4, image_id: 2, purpose: nil},
+  {room_id: 4, image_id: 3, purpose: nil},
+  {room_id: 5, image_id: 3, purpose: "cover"},
+  {room_id: 5, image_id: 4, purpose: nil},
+  {room_id: 5, image_id: 5, purpose: nil},
+  {room_id: 6, image_id: 6, purpose: "cover"},
+  {room_id: 6, image_id: 7, purpose: nil},
+  {room_id: 6, image_id: 8, purpose: nil},
+  {room_id: 7, image_id: 9, purpose: "cover"},
+  {room_id: 7, image_id: 4, purpose: nil},
+  {room_id: 7, image_id: 10, purpose: nil},
+  {room_id: 3, image_id: 1, purpose: nil},
+  {room_id: 5, image_id: 9, purpose: nil},
+  {room_id: 4, image_id: 3, purpose: nil},
+  {room_id: 6, image_id: 4, purpose: nil},
+  {room_id: 7, image_id: 2, purpose: nil}
+])
 Issue.create!([
   {category_id: 1, user_id: 1, text: "example", status: "opened"}
 ])
-Room.create!([
-  {capacity: 2, price: 3.0, number: "34a", title: "Twin Guest Room", slug: nil},
-  {capacity: 4, price: 5.0, number: "34t", title: "King Guest Room", slug: nil},
-  {capacity: 4, price: 3.0, number: "3", title: "King Deluxe Room", slug: nil}
-])
-RoomImage.create!([
-  {room_id: "1.0", image_id: "1.0", purpose: "cover"},
-  {room_id: "2.0", image_id: "2.0", purpose: "cover"},
-  {room_id: "3.0", image_id: "3.0", purpose: "cover"},
-  {room_id: "1.0", image_id: "3.0", purpose: nil},
-  {room_id: "1.0", image_id: "4.0", purpose: nil},
-  {room_id: "1.0", image_id: "5.0", purpose: nil},
-  {room_id: "2.0", image_id: "6.0", purpose: nil},
-  {room_id: "2.0", image_id: "7.0", purpose: nil},
-  {room_id: "3.0", image_id: "8.0", purpose: nil},
-  {room_id: "3.0", image_id: "9.0", purpose: nil},
-  {room_id: "2.0", image_id: "4.0", purpose: nil},
-  {room_id: "3.0", image_id: "10.0", purpose: nil}
+Order.create!([
+  {check_in: "2021-07-02", check_out: "2021-07-05", total_price: 100.0, room_id: 1, user_id: 1, status: "processed"},
+  {check_in: "2021-07-01", check_out: "2021-07-04", total_price: 150.0, room_id: 2, user_id: 2, status: "confirmed"},
+  {check_in: "2021-07-15", check_out: "2021-07-20", total_price: 200.0, room_id: 3, user_id: 1, status: "completed"},
+  {check_in: "2021-07-20", check_out: "2021-07-25", total_price: 100.0, room_id: 2, user_id: 2, status: "cancelled"},
+  {check_in: "2021-07-20", check_out: "2021-07-25", total_price: 100.0, room_id: 2, user_id: 2, status: nil},
+  {check_in: "2021-07-20", check_out: "2021-07-25", total_price: 100.0, room_id: 1, user_id: 2, status: "confirmed"}
 ])
 RoomService.create!([
   {room_id: 1, service_id: 1},
@@ -50,21 +66,52 @@ RoomService.create!([
   {room_id: 3, service_id: 4},
   {room_id: 3, service_id: 5},
   {room_id: 2, service_id: 6},
-  {room_id: 3, service_id: 7},
-  {room_id: 2, service_id: 4}
+  {room_id: 1, service_id: 8},
+  {room_id: 2, service_id: 4},
+  {room_id: 4, service_id: 2},
+  {room_id: 4, service_id: 3},
+  {room_id: 5, service_id: 8},
+  {room_id: 4, service_id: 1},
+  {room_id: 5, service_id: 2},
+  {room_id: 5, service_id: 6},
+  {room_id: 6, service_id: 4},
+  {room_id: 6, service_id: 4},
+  {room_id: 7, service_id: 5},
+  {room_id: 7, service_id: 7},
+  {room_id: 3, service_id: 3},
+  {room_id: 1, service_id: 4},
+  {room_id: 1, service_id: 5},
+  {room_id: 3, service_id: 6},
+  {room_id: 2, service_id: 7},
+  {room_id: 2, service_id: 8},
+  {room_id: 3, service_id: 8},
+  {room_id: 1, service_id: 7}
+])
+Feedback.create!([
+  {text: "example", user_id: 1, stars: 4}
+])
+Room.create!([
+  {capacity: 2, price: 3.99, number: "34a", title: "Twin Guest Room", slug: nil},
+  {capacity: 4, price: 5.0, number: "34t", title: "King Guest Room", slug: nil},
+  {capacity: 4, price: 3.0, number: "3", title: "King Deluxe Room", slug: nil},
+  {capacity: 3, price: 4.99, number: "23", title: "Twin Guest Room", slug: nil},
+  {capacity: 1, price: 5.0, number: "11", title: "King Guest Room", slug: nil},
+  {capacity: 2, price: 2.5, number: "30b", title: "King Deluxe Room", slug: nil},
+  {capacity: 3, price: 4.99, number: "56", title: "Twin Guest Room", slug: nil}
 ])
 Service.create!([
-  {name: "TV", description: "", icon_url: nil},
-  {name: "Refrigerator", description: "Example", icon_url: nil},
-  {name: "Hair dryer", description: "", icon_url: nil},
-  {name: "Mini bar", description: "Example", icon_url: nil},
-  {name: "Safe", description: "", icon_url: nil},
-  {name: "Air conditioner", description: "", icon_url: nil},
-  {name: "Wi-FI", description: "Example", icon_url: "Icons/wifi.png"}
+  {name: "Wi-FI", description: "Example", icon_url: "Icons/wifi.png"},
+  {name: "TV", description: "", icon_url: "Icons/tv.png"},
+  {name: "Refrigerator", description: "Example", icon_url: "Icons/fridge.png"},
+  {name: "Hair dryer", description: "", icon_url: "Icons/hairdryer.png"},
+  {name: "Mini bar", description: "Example", icon_url: "Icons/bar.png"},
+  {name: "Safe", description: "", icon_url: "Icons/safe.png"},
+  {name: "Electric kettle", description: "", icon_url: "Icons/kettle.png"},
+  {name: "Air conditioner", description: "", icon_url: "Icons/conditioner.png"}
 ])
 Category.create!([
   {name: "Advice", description: "You can leave a word of advice in this section, help us to become even better!"},
-  {name: "Breakage", description: "In this section, you can report any kind of breakdown in your room!"},
-  {name: "Urgent", description: "If you have something you want us to react quickly, please type it here!"},
+  {name: "Breakage", description: "In this section you can report any kind of breakdown in your room"},
+  {name: "Urgent", description: "If you have something you want us to react quickly, please type it here"},
   {name: "Other", description: ""}
 ])
