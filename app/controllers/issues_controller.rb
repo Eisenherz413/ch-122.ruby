06d69c7f1398f60c299ed5ab963f3215380f7b57
anastass413 <anastass413@gmail.com>
2021-09-01 15:37:15 +0300

    Issues are shown for their owners only Category name is displayed instead of its id new issue form is upgraded with JS

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -25,8 +25,8 @@ class IssuesController < ApplicationController
 
     respond_to do |format|
       if @issue.save
-        format.html { redirect_to @issue, notice: "Issue was successfully created." }
-        format.json { render :show, status: :created, location: @issue }
+        format.html { redirect_to issues_url, notice: "Issue was successfully created." }
+        format.json { render :index, status: :created, location: @issue }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @issue.errors, status: :unprocessable_entity }
