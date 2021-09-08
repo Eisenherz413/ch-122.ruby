f56a229a03eb3c855bfbcd08480d15357408c981
anastass413 <anastass413@gmail.com>
2021-09-08 23:11:36 +0300

    Simple sorting by clicking on column name added

--- app/controllers/issues_managers_controller.rb
+++ app/controllers/issues_managers_controller.rb
@@ -2,7 +2,7 @@ class IssuesManagersController < ApplicationController
 
   # GET /issues_managers or /issues_managers.json
   def index
-    @issues = Issue.all.paginate(:per_page => 5, :page => params[:page])
+    @issues = Issue.all.order(params[:sort]).paginate(:per_page => 5, :page => params[:page])
   end
 
 end
