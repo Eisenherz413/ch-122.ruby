01104f987008a04df5b3597ab9487b38996b6753
anastass413 <anastass413@gmail.com>
2021-09-09 21:30:02 +0300

    Pagination is now more compact and sticks to bottom. Urgent issues display first

--- app/controllers/issues_managers_controller.rb
+++ app/controllers/issues_managers_controller.rb
@@ -2,17 +2,17 @@ class IssuesManagersController < ApplicationController
   helper_method :sort_column, :sort_direction
   # GET /issues_managers or /issues_managers.json
   def index
-    @issues = Issue.order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
+    @issues = Issue.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 7)
   end
 
   private
 
   def sort_column
-    Issue.column_names.include?(params[:sort]) ? params[:sort] : "category_id"
+    Issue.column_names.include?(params[:sort]) ? params[:sort] : 'category_id'
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
 end
