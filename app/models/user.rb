baf7a5bb38e0a60d69ec9b038e68ac9e1384fdd0
Olha-30 <vasianovych.olha@chnu.edu.ua>
2021-09-13 10:38:49 +0300

    correct user fileds in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   enum role: %i[user manager admin], _default: 'user'
   enum status: %i[active blocked], _default: 'active'
 
-  validates :full_name, uniqueness: { case_sensitive: false }, length: { minimum: 2 }, presence: true
+  validates :full_name, length: { minimum: 2 }, presence: true
   validates :email, format: {with: /\A[a-z0-9+_.-]+@[a-z0-9.-]+\z/}, presence: true
-  validate_enum_attributes :role, message: "Role is not a valid"
-  validate_enum_attributes :status, message: "Status is not a valid"
+  validate_enum_attributes :role
+  validate_enum_attributes :status
 
 
   has_many :issues
