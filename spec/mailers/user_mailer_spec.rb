5a25dc9bcafdaf5ecf6aac3d09ccbc6e0966a4f7
schernaya <chernaya.sofiia@gmail.com>
2021-09-15 12:51:14 +0300

    fix search, add test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify" do
+    let(:mail) { UserMailer.new_order_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Booking confirmation")
+      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.from).to eq(["rubygem.hotel@gmail.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
 end
