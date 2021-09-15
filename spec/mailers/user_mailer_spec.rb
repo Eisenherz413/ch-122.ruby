05fc0f23402a3884de71c84e8c7b4b4412aa7e54
schernaya <chernaya.sofiia@gmail.com>
2021-09-15 13:08:04 +0300

    create user in mailer test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "notify" do
-    let(:mail) { UserMailer.new_order_email }
-
+    let(:user) { create(user: { full_name: "Sophia", email: "chernaya.sofiia@gmail.com" }) }
+    let(:mail) { UserMailer.with(email: user.email).new_order_email }
+    
     it "renders the headers" do
       expect(mail.subject).to eq("Booking confirmation")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq(user.email)
       expect(mail.from).to eq(["rubygem.hotel@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("We have already started preparing for your arrival!")
     end
   end
 end
