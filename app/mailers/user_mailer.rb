0a70bfda67628b27c04ffc289bb47f0ce9b717e3
schernaya <chernaya.sofiia@gmail.com>
2021-09-15 09:29:01 +0300

    add mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,10 @@ class UserMailer < ApplicationMailer
   def new_order_email
     @order = params[:order]
     @email = params[:email]
-    mail(to: @email, subject: "You got a new order!")
+
+    timeDiff = (@order.check_out - @order.check_in).abs
+    @numberOfNights = (timeDiff / (1000 * 3600 * 24)).ceil
+
+      mail(to: @email, subject: "You got a new order!")
   end
 end
